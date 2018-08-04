0d435bc8d3f50912eb398788c24383b20846a99c
Brian Maurer <bmaurer@localhost.localdomain>
2018-08-04 05:11:02 -0400

    first commit

---
+++ aa4s.py
@@ -0,0 +1,91 @@
+"""
+Anime Adapter for Sonarr (aa4s)
+"""
+
+import tvdb_api
+from Pymoe import Anilist
+import requests
+
+TVDB_API = tvdb_api.Tvdb()
+ANILIST_API = Anilist()
+ANILIST_URL = "https://graphql.anilist.co"
+ANILIST_HEADER = {
+	"Content-Type": "application/json",
+	"User-Agent": "aa4s",
+	"Accept": "application/json"
+}
+ANILIST_RELATIONS = [
+	"PREQUEL",
+	"SEQUEL",
+	"SIDE_STORY",
+	"SUMMARY"
+]
+
+# Testing variables
+__tvdb_id = 281251
+__anilist_id = 5081
+
+def fetch_tvdb_seasons(tvdb_id):
+	seasons = dict()
+	for key in TVDB_API[tvdb_id].keys():
+		seasons.update({key: TVDB_API[tvdb_id][key]})
+	return seasons
+
+def fetch_anilist_show(anilist_id):
+	query_string = """\
+		query($id: Int) {
+			Media(id: $id, type: ANIME) {
+				id
+				title {
+					romaji
+				}
+				startDate {
+					year
+					month
+					day
+				}
+				endDate {
+					year
+					month
+					day
+				}
+				episodes
+				relations {
+					edges {
+						relationType
+					}
+					nodes {
+						id
+					}
+				}
+			}
+		}
+	"""
+	vars = {"id": anilist_id}
+	request = requests.post(ANILIST_URL, headers=ANILIST_HEADER, json={"query": query_string, "variables": vars})
+	json = {}
+	try:
+		json = request.json()
+	except:
+		print("ERROR!")
+		return
+	return json
+
+def get_ids_in_set(set_var):
+	ids = []
+	for element in set_var:
+		ids.append(element['id'])
+	return ids
+
+def fetch_anilist_seasons(anilist_id):
+	anime = []
+	anime.append(fetch_anilist_show(anilist_id)['data']['Media'])
+	for show in anime:
+		for index, value in enumerate(show['relations']['nodes']):
+			if show['relations']['edges'][index]['relationType'] in ANILIST_RELATIONS and value['id'] not in get_ids_in_set(anime):
+				anime.append(fetch_anilist_show(value['id'])['data']['Media'])
+	return anime
+
+# fetch_tvdb_seasons(281251)
+print(fetch_anilist_seasons(__anilist_id))
+
