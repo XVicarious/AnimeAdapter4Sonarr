1b30717eb618e031d309c3d42a6103e37e1003a8
Brian Maurer <bmaurer@xvicario.us>
2018-08-27 22:00:13 -0400

    Rewrite everything.

--- aa4s.py
+++ aa4s.py
@@ -2,217 +2,106 @@
 Anime Adapter for Sonarr (aa4s)
 """
 
-import datetime
-import pprint
-import logging
-import pickle
-from difflib import SequenceMatcher
-import tvdb_api
+import os
+import sys
+import xml.etree.ElementTree as ET
+import re
+
 import requests
-from slugify import slugify
+from Pymoe import Kitsu
 
 
-from kitsu import Kitsu
+XEM_URL =\
+    "http://thexem.de/map/all?id={origin_id}&origin={origin_name}&destination={destination_name}"
+__TVDB_ID = 81831
+EPISODE_NUMBERING = r"S(\d*)E(\d*)"
 
-#logging.basicConfig(level=logging.DEBUG)
-
-TVDB_API = tvdb_api.Tvdb()
-
-KITSU = Kitsu("dd031b32d2f56c990b1425efe6c42ad847e7fe3ab46bf1299f05ecd856bdb7dd", "54d7307928f63414defd96399fc31ba847961ceaecef3a5fd93144e960c0e151")
-
-WANTED_KITSU_ROLES = [
-    KITSU.Role.PARENT_STORY,
-    KITSU.Role.SEQUEL,
-    KITSU.Role.PREQUEL,
-    KITSU.Role.SIDE_STORY,
-    # KITSU.Role.ADAPTATION
-]
-
-PP = pprint.PrettyPrinter(indent=2)
-
-# Testing variables
-__tvdb_id = 81831
-__anilist_id = 3455
-__anidb_id = 5625
-__kitsu_id = 3021
+KITSU = Kitsu("dd031b32d2f56c990b1425efe6c42ad847e7fe3ab46bf1299f05ecd856bdb7dd",
+              "54d7307928f63414defd96399fc31ba847961ceaecef3a5fd93144e960c0e151")
 
 
-def fetch_kitsu_seasons(kitsu_id):
-    """ Fetch all the shows on kitsu that are related """
-    all_animes = []
-
-    def __id_in_all(needle):
-        for anime in all_animes:
-            if anime['id'] == needle:
-                return True
-        return False
-
-    episodes = KITSU.get_anime_episodes(kitsu_id)
-    all_animes.append({'id': kitsu_id, 'episodes': episodes})
-    i = 0
-    while i < len(all_animes):
-        current_relations = KITSU.get_anime_relationship_ids(all_animes[i]['id'], roles=WANTED_KITSU_ROLES)
-        for kid in current_relations:
-            if not __id_in_all(kid):
-                all_animes.append({
-                    'id': kid,
-                    'episodes': KITSU.get_anime_episodes(kid)
-                })
-        i += 1
-    return all_animes
+class ShowNotMappedException(Exception):
+    """ Raised when the requested show is not found on XEM """
+    def __init__(self, origin, showid):
+        super().__init__("XEM does not have the mapping for {} id {}".format(origin, showid))
 
 
-def clean_tvdb_seasons(tvdb_seasons):
-    """Clean up unneeded data on the seasons fetched from TVDB"""
-    clean_seasons = dict()
-    for index, season in tvdb_seasons.items():
-        clean_episodes = dict()
-        for episode_index, episode in season.items():
-            clean_episodes.update({episode_index: {
-                'episodeName': episode['episodeName'],
-                'airDate': episode['firstAired']
-            }})
-        clean_seasons.update({index: clean_episodes})
-    return clean_seasons
+class EpisodeNotMappedException(Exception):
+    """ Raised when the requested episode has not been mapped on XEM """
+    def __init__(self, season, episode):
+        super().__init__("Season {}, Episode {} is not mapped on XEM!".format(season, episode))
 
 
-def fetch_tvdb_seasons(tvdb_id):
-    """Return all seasons of the given show."""
-    seasons = dict()
-    for key in TVDB_API[tvdb_id].keys():
-        seasons.update({key: TVDB_API[tvdb_id][key]})
-    # print("{} has {} seasons".format(tvdb_id, len(seasons)))
-    return clean_tvdb_seasons(seasons)
+def get_all_names_from_xem(origin="tvdb", season=1):
+    xem_names = requests.get("http://thexem.de/map/allNames?origin={}&season={}".format(origin, season))
+    if xem_names.status_code != 200:
+        raise Exception("Status code is not 200! Something is wrong!")
+    json = xem_names.json()
+    if json['result'] == "success":
+        return json['data']
+    raise Exception("I don't know what happened!")
 
 
-def get_ids_in_set(set_var):
-    """Return the ids for all of the elements in a given array."""
-    ids = []
-    for element in set_var:
-        ids.append(element['id'])
-    return ids
+def get_romaji_name_from_tvdb(tvdbid):
+    kitsu_req = requests.get("https://kitsu.io/api/edge/mappings?filter[externalSite]=thetvdb/series&filter[externalId]={}".format(tvdbid))
+    if kitsu_req.status_code != 200:
+        raise Exception("Status code is not 200! Something is wrong!")
+    item_link = "https://kitsu.io/api/edge/mappings/{}/item".format(kitsu_req.json()['data'][0]['id'])
+    print(item_link)
 
 
-def dates_within_n_days(date1, date2, n):
-    """ Are the dates within n days of each other """
-    if abs((date1 - date2).days) < n + 1:
-        return True
-    return False
+def get_xem_map(origin_name, origin_id, destination_name):
+    request = requests.get(
+        XEM_URL.format(
+            origin_name=origin_name, origin_id=origin_id, destination_name=destination_name))
+    if request.status_code != 200:
+        raise Exception("Status code is not 200! Something is wrong!")
+    json = request.json()
+    if json['result'] == "success":
+        return json['data']
+    raise ShowNotMappedException(origin_name, origin_id)
 
 
-def map_tvdb_to_kitsu(tvdb_seasons, kitsu_seasons, current_map):
-    """ Map TVDB episodes to kitsu episodes """
-    mapped_episodes = dict() if current_map is None else current_map
+def find_mapped_episde(xemmap, source, destination, season, episode):
+    for mapped_episode in xemmap:
+        if season == mapped_episode[source]["season"]\
+                and episode == mapped_episode[source]["episode"]:
+            return [mapped_episode[destination]["season"],
+                    mapped_episode[destination]["episode"]]
+    raise EpisodeNotMappedException(season, episode)
 
-    def __is_mapped(season, episode):
-        for i, mapped in mapped_episodes.items():
-            for m in mapped:
-                if [season, episode] in m:
-                    return True
-        return False
 
-    def __is_mapped_to_season(kitsu_id, season, episode):
-        for m in mapped_episodes[kitsu_id]:
-            if [season, episode] in m:
-                return True
-        return False
-
-    def __is_kitsu_mapped(kitsu_id, episode):
-        for m in mapped_episodes[kitsu_id]:
-            if episode in m:
-                return True
-        return False
-
-    stored_kitsu = None
-    for season in kitsu_seasons:
-        kitsu_id = int(season['id'])
-        if kitsu_id not in mapped_episodes.keys():
-            mapped_episodes.update({kitsu_id: []})
-
-        for episode in season['episodes']:
-            kitsu_episode = episode['attributes']['number']
-            print("--------------------{}/{}--------------------".format(kitsu_id, kitsu_episode))
-            if __is_kitsu_mapped(kitsu_id, kitsu_episode):
-                stored_kitsu = episode
-                continue
-            solved = False
-            kitsu_episode = episode['attributes']['number']
-            # print("{}-{}".format(kitsu_id, kitsu_episode), episode['attributes']['seasonNumber'])
-            airdate = datetime.datetime.strptime("1970-1-1", "%Y-%m-%d")
+def get_anime_folders(anime_library):
+    xem_map = None
+    tvdbid = None
+    # anidbid = None  # todo: use this for finding specials, maybe?
+    for root, dirs, files in os.walk(anime_library):
+        path = root.split(os.sep)
+        if "tvshow.nfo" in files:
+            tree = ET.parse(os.path.join(root, "tvshow.nfo"))
+            eroot = tree.getroot()
             try:
-                airdate = datetime.datetime.strptime(episode['attributes']['airdate'], "%Y-%m-%d")
-            except TypeError:
-                " "
-                # print("Kitsu episode {}-{} doesn't have an airdate. We'll try to compare the episode names instead.".format(season['id'], episode['attributes']['number']))
-            episode_name = ""
-            episode_names = []
-            try:
-                episode_name = slugify(episode['attributes']['canonicalTitle'])
-                episode_names = [slugify(x.strip()) for x in episode['attributes']['canonicalTitle'].split("/")]
-            except: # todo: add exception type
-                " "
-                # print("no episode name")
-            for season_index, tvdb_season in tvdb_seasons.items():
-                for episode_index, tvdb_episode in tvdb_season.items():
-                    if __is_mapped(season_index, episode_index):
-                        continue
-                    tvdb_airdate = datetime.datetime.strptime("1970-1-1", "%Y-%m-%d")
-                    try:
-                        tvdb_airdate = datetime.datetime.strptime(tvdb_episode['airDate'], "%Y-%m-%d")
-                    except: # todo: add exception type
-                        " "
-                    tvdb_name = slugify(tvdb_episode['episodeName'])
-                    tvdb_names = [slugify(x.strip()) for x in tvdb_episode['episodeName'].split("/")]
-                    ratio = SequenceMatcher(None, episode_name, tvdb_name).ratio()
-                    ratios = []
-                    big_index = len(episode_names) if len(episode_names) < len(tvdb_names) else len(tvdb_names)
-                    o = 0
-                    while o < big_index:
-                        ratios.append(str(SequenceMatcher(None, episode_names[o], tvdb_names[o]).ratio()) + "," + episode_names[o] + "," + tvdb_names[o])
-                        o += 1
-                    average = 0.0
-                    if len(ratios) > 1:
-                        for ro in ratios:
-                            average += float(ro.split(",")[0])
-                    try:
-                        average = average / len(ratios)
-                    except: # todo: add exception type
-                        average = 0
-                    if dates_within_n_days(airdate, tvdb_airdate, 1) or episode_name == tvdb_name:
-                        print("PASS ({}): {} == {}".format(ratio, episode_name, tvdb_name))
-                        mapped_episodes[kitsu_id].append([[season_index, episode_index], episode['attributes']['number']])
-                        solved = True
+                tvdbid = eroot.find("tvdbid").text
+                xem_map = get_xem_map("tvdb", tvdbid, "anidb")
+            except ShowNotMappedException as snm:
+                print(snm)
+            except AttributeError:
+                print("{} does not have a TVDB id!".format(path[len(path) - 1]))
+        elif len(files) > 1 and "season.nfo" in files:
+            for file in files:
+                if file.endswith("mkv") or file.endswith("mp4"):
+                    matches = re.search(EPISODE_NUMBERING, file)
+                    if len(matches.groups()) >= 2:
+                        episode = None
+                        try:
+                            episode = find_mapped_episde(xem_map, "tvdb", "anidb",
+                                                         int(matches[1]), int(matches[2]))
+                        except EpisodeNotMappedException as enm:
+                            print(enm)
                         break
-                    else:
-                        # print("Matching failed. Details here:")
-                        if ratio >= 0:
-                            print("FAIL ({}): {} != {}".format(ratio, episode_name, tvdb_name))
-                            if len(ratios) > 1:
-                                print("EXTRA: AVG {}".format(average))
-                                print("EXTRA: {}".format(ratios))
-                        # print("K-{}-{}: {} aired on {}".format(kitsu_id, kitsu_episode, episode_name, airdate))
-                        # print("T-{}-{}: {} aired on {}".format(season_index, episode_index, slugify(tvdb_episode['episodeName']), tvdb_date))
-                    if __is_mapped_to_season(kitsu_id, season_index, episode_index - 1):
-                        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                        print("LAST EPISODE FOR THIS SEASON IS IN THE KITSU SEASON!")
-                        print("{}-{} might match to kitsu {}-{}".format(season_index, episode_index, kitsu_id, kitsu_episode))
-                        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                if solved:
-                    break
-            stored_kitsu = episode
-    return mapped_episodes
 
 
-# __tvdb_id = 102261
-# __kitsu_id = KITSU.get_from_kitsu_map(KITSU.Mapping.TVDB_SERIES, __tvdb_id)[0]
-the_map = None
-try:
-    the_map = pickle.load(open(str(__tvdb_id) + ".pkl", 'rb'))
-except:
-    print("Map not found for id, starting over")
-the_map = map_tvdb_to_kitsu(
-    fetch_tvdb_seasons(__tvdb_id),
-    fetch_kitsu_seasons(__kitsu_id),
-    the_map
-)
-pickle.dump(the_map, open(str(__tvdb_id) + ".pkl", 'wb'))
+print(sys.argv[1])
+# print(sys.argv[2])
+#get_romaji_name_from_tvdb(__TVDB_ID)
+get_anime_folders(str(sys.argv[1]))
